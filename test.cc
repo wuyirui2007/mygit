8673a22baa96bdce138fb76be2e92f4a37bfcd23
wuyirui2007 <496492275@qq.com>
2017-06-03 22:14:56 +0800

    test

---
+++ test.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    test.cc
+ /// @author  lemon(wuqiujun)
+ /// @date    2017-06-03 22:13:36
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout<<"this is a test"<<endl;
+	return 0;
+}
